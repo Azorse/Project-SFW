d9226cddc19eb35f4e5108569398d9a8fb8c1d24
Andrew Hardemon <aeroatlas@ymail.com>
2020-03-05 21:12:31 -0700

    Got the barebones website, components, controllers, models, routes, and api files

---
+++ client/src/components/Button/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+function Button(props) {
+  return (
+    <span className="button-btn" {...props} role="button" tabIndex="0">
+      ✗
+    </span>
+  );
+}
+
+export default Button;
