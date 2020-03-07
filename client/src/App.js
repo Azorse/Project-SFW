a600aff5b5e4dbb8fea78fe778243a6738cf15e9
Dan Schissler <schisslerdg@gmail.com>
2020-03-07 12:49:55 -0800

    register page

--- client/src/App.js
+++ client/src/App.js
@@ -28,6 +28,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Register from "./pages/Register"
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -40,6 +41,7 @@ function App() {
           <Route exact path="/" component={Home} />
           {/* <Route exact path="/books" component={Books} />
           <Route exact path="/books/:id" component={Detail} /> */}
+          <Route exact path="/register" component={Register} />
           <Route component={NoMatch} />
         </Switch>
       </div>
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,13 @@ const db = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
-  findAll: function(req, res) {
-    db.Book
-      .find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
+  // findAll: function(req, res) {
+  //   db.Book
+  //     .find(req.query)
+  //     .sort({ date: -1 })
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // },
   findById: function(req, res) {
     db.Book
       .findById(req.params.id)
@@ -16,19 +16,19 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.User
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.User
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
