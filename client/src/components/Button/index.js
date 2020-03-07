5e9e0aaed318de641833dc4195554351c788b216
Andrew Hardemon <aeroatlas@ymail.com>
2020-03-07 10:40:46 -0700

    Minor button change and adding login page

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 // That way we don't have to define them all individually
 function Button(props) {
   return (
-    <span className="button-btn" {...props} role="button" tabIndex="0">
+    <span className="delete-btn" {...props} role="button" tabIndex="0">
       ✗
     </span>
   );
