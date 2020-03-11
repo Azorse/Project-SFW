6aa44d708f1a387149d04a508824cbfa33a6b169
DStark <hindash@email.arizona.edu>
2020-03-10 21:48:31 -0700

    less conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LogIn from "./pages/LogIn";
 import Home from "./pages/Home";
-import Register from "./pages/Register"
+import Register from "./pages/Register";
 // import Standings from "./pages/Standings"
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import Standings from "./pages/Standings";
 
 function App() {
   return (
@@ -16,7 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={LogIn} />
           <Route exact path="/home" component={Home} />
-          <Route path="/standings" component={Standings} />
+          {/* <Route path="/standings" component={Standings} /> */}
           <Route exact path="/register" component={Register} />
           <Route component={NoMatch} />
         </Switch>
