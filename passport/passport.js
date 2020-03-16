7186ec1a10a7b1ff301bc13a8499b6d642f315ff
Dan Schissler <schisslerdg@gmail.com>
2020-03-15 17:25:31 -0700

    fixing requires and home.js

--- passport/passport.js
+++ passport/passport.js
@@ -1,9 +1,7 @@
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
-const jwtSecret = require('./jwtConfig');
 const LocalStrategy = require('passport-local').Strategy;
-const JWTstrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
+
 
 const User = require('../models/User');
 
