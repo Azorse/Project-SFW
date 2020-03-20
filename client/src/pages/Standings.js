0721f0a1ec6b1f37ca4ef79da2fe2fb24d81b1f2
Daniel Joji Koons <jojikoons@gmail.com>
2020-03-19 21:17:31 -0700

    Graphs

--- client/src/pages/Standings.js
+++ client/src/pages/Standings.js
@@ -34,31 +34,31 @@ class Chart extends Component {
       images: [
         {
           name: Gryffindor,
-          value: "gryffindor",
+          value: "Gryffindor",
           data:
             "The Gryffindor house emphasises the traits of courage as well as daring, nerve, and chivalry, and thus its members are generally regarded as brave, though sometimes to the point of recklessness. Some Gryffindors have also been noted to be short-tempered."
         },
         {
           name: Hufflepuff,
-          value: "hufflepuff",
+          value: "Hufflepuff",
           data:
             "Students belonging to this house are known to be hard-working, friendly, loyal, honest and rather impartial. It may be that due to their values, Hufflepuffs are not as competitive as the other houses, and are more modest about their accomplishments. Hufflepuff is the most inclusive among the four houses; valuing hard work, dedication, patience, loyalty, and fair play rather than a particular aptitude in its students."
         },
         {
           name: Ravenclaw,
-          value: "ravenclaw",
+          value: "Ravenclaw",
           data:
             "Ravenclaw House prizes learning, wisdom, wit, and intellect in its members. Thus, many Ravenclaws tend to be academically motivated and talented students. They also pride themselves on being original in their ideas, and methods. It's not unusual to find Ravenclaw students practising especially different types of magic that other houses might shun."
         },
         {
           name: Slytherin,
-          value: "slytherin",
+          value: "Slytherin",
           data:
             "Slytherins tend to be ambitious, shrewd, cunning, strong leaders, and achievement-oriented. They also have highly developed senses of self-preservation. This means that Slytherins tend to hesitate before acting, so as to weigh all possible outcomes before deciding exactly what should be done."
         },
         {
           name: Hogwarts,
-          value: "hogwarts",
+          value: "Hogwarts",
           data:
             "You have yet to be sorted"
         }
@@ -76,28 +76,38 @@ class Chart extends Component {
     this.loggedIn();
      quizAPI.getGryff()
      .then(res => {
-      const x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      const x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       res.data.forEach(el => {
         el.questions.map(ele => {
           // ravenRight.push(ele)
-          console.log(ele)
+          // console.log(ele)
           
           for(var j = 1; j < Object.values(ele).length; j++){
             console.log(Object.values(ele)[j])
-            x[j] += Object.values(ele)[j]
+            x[j - 1] += Object.values(ele)[j]
           }
-          console.log(x)
+          // console.log(x)
           // Object.keys(ele).forEach(x => console.log(Object.values(ele)))
         })
-        console.log(x)
+        // console.log(x)
+        
       })
       chartData.push(
         {
-        labels: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10"],
+        labels: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10", "Zero Axes"],
         datasets: [{ data: x,
                     backgroundColor: "rgba(139, 23, 23, 0.5)",
                     label:"Gryffindor"
-                                    }]
+                                    }],
+          options:{
+            scales: {
+                yAxes : [{
+                    ticks : { 
+                        min : 0
+                    }
+                }]
+            }
+        }
       })
       this.setState({ chartData });
     })
@@ -107,28 +117,38 @@ class Chart extends Component {
     //Get Slytherin
     quizAPI.getSlyth()
     .then(res => {
-      const x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      const x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       res.data.forEach(el => {
         el.questions.map(ele => {
           // ravenRight.push(ele)
-          console.log(ele)
+          // console.log(ele)
           
           for(var j = 1; j < Object.values(ele).length; j++){
             console.log(Object.values(ele)[j])
-            x[j] += Object.values(ele)[j]
+            x[j - 1] += Object.values(ele)[j]
           }
-          console.log(x)
+          // console.log(x)
           // Object.keys(ele).forEach(x => console.log(Object.values(ele)))
         })
-        console.log(x)
+        // console.log(x)
       })
       chartData.push(
         {
-        labels: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10"],
+        labels: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10", "Zero Axes"],
         datasets: [{ data: x,
                     backgroundColor: "rgba(32, 103, 29, 0.5)",
                     label:"Slytherin"
-                                    }]
+                                    }],
+        options:{
+          scales: {
+              yAxes : [{
+                  ticks : {
+                      max : 1,    
+                      min : -1
+                  }
+              }]
+          }
+      }
       })
       this.setState({ chartData });
     })
@@ -137,28 +157,34 @@ class Chart extends Component {
 
     quizAPI.getRaven()
     .then(res => {
-      const x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      const x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       res.data.forEach(el => {
         el.questions.map(ele => {
           // ravenRight.push(ele)
           console.log(ele)
-          
-          for(var j = 1; j < Object.values(ele).length; j++){
-            console.log(Object.values(ele)[j])
-            x[j] += Object.values(ele)[j]
+          var objV = Object.values(ele)
+          for(var j = 1; j < objV.length; j++){
+            console.log(objV[j])
+            x[j - 1] += objV[j]
           }
           console.log(x)
           // Object.keys(ele).forEach(x => console.log(Object.values(ele)))
         })
-        console.log(x)
-      })
+   
+      })    
       chartData.push(
         {
-        labels: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10"],
+        labels: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10", "Zero Axes"],
         datasets: [{ data: x,
                     backgroundColor: "rgba(11, 37, 141, 0.5)",
-                    label:"Ravenclaw"
-                                    }]
+                    label:"Ravenclaw",
+
+                  }],
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
       })
       this.setState({ chartData });
     })
@@ -166,7 +192,7 @@ class Chart extends Component {
 
     quizAPI.getHuff()
     .then(res => {
-      const x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      const x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       res.data.forEach(el => {
         el.questions.map(ele => {
           // ravenRight.push(ele)
@@ -174,16 +200,16 @@ class Chart extends Component {
           
           for(var j = 1; j < Object.values(ele).length; j++){
             console.log(Object.values(ele)[j])
-            x[j] += Object.values(ele)[j]
+            x[j - 1] += Object.values(ele)[j]
           }
-          console.log(x)
+          // console.log(x)
           // Object.keys(ele).forEach(x => console.log(Object.values(ele)))
         })
-        console.log(x)
+        // console.log(x)
       })
       chartData.push(
         {
-        labels: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10"],
+        labels: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10", "Zero Axes"],
         datasets: [{ data: x,
                     backgroundColor: "rgba(202, 183, 6, 0.5)",
                     label:"Hufflepuff"
@@ -243,7 +269,7 @@ class Chart extends Component {
 
 
   render() {
-    const {id, house} = this.props.location.state
+    const {id, house} = this.state
     return (
             <Container fluid>
             <Nav />
@@ -259,12 +285,12 @@ class Chart extends Component {
                 <Col size="md-12">
                 <div className="chart">
                   {this.state.chartData.map((n, index) => {
-                    return <Bar key={index} data={n} />;
+                    return <Bar key={index} data={n}/>;
                   })}
                 </div>
-                </Col>
-              </Row>
-            </Container>
+             </Col>
+            </Row>
+          </Container>
       
     );
   }
